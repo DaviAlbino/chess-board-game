a30cdb357ecc1883f72536972cd826b37b6b2528
Davi Albino <negroalbino_@hotmail.com>
2023-08-23 17:49:02 -0300

    feat: Position class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boradgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Chess players");
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+
+	}
+
+}

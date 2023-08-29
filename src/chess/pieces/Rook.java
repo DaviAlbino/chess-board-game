426ea125fd5ce6fa04c95da4b2195498b14dd6dc
Davi Albino <negroalbino_@hotmail.com>
2023-08-29 13:04:19 -0300

    feat: possible moves for Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boradgame.Board;
+import boradgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -19,7 +20,53 @@ public class Rook extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] possible = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position auxPosition = new Position(0, 0);
+		
+		// above
+		auxPosition.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setRow(auxPosition.getRow() - 1);
+		}
+		if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// left
+		auxPosition.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setColumn(auxPosition.getColumn() - 1);
+		}
+		if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// right
+		auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setColumn(auxPosition.getColumn() + 1);
+		}
+		if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// below
+		auxPosition.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setRow(auxPosition.getRow() + 1);
+		}
+		if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		
 		return possible;
 	}
+	
+	
 	
 }

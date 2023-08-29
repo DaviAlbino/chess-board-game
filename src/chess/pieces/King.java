7560d2a6335731e8861c3f28c8adbe3f6d34beca
Davi Albino <negroalbino_@hotmail.com>
2023-08-29 13:43:17 -0300

    feat: king possible moves

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boradgame.Board;
+import boradgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,10 +16,66 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece)getBoard().piece(position);
+		return piece == null || piece.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] possible = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position auxPosition = new Position(0, 0);
+		
+		// above
+		auxPosition.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// below
+		auxPosition.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// left
+		auxPosition.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// right
+		auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// ne
+		auxPosition.setValues(position.getRow() -1, position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// nw
+		auxPosition.setValues(position.getRow() -1, position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// sw
+		auxPosition.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		// se
+		auxPosition.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			possible[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
 		return possible;
 	}
 	

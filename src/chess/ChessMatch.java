a8062c7aac58cbb03cb5ea571a90ac306e471c27
Davi Albino <negroalbino_@hotmail.com>
2023-08-30 18:17:02 -0300

    feat: piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -86,7 +86,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece sourcePiece = board.removePiece(source);
+		ChessPiece sourcePiece = (ChessPiece)board.removePiece(source);
+		sourcePiece.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(sourcePiece, target);
 		
@@ -99,7 +100,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece piece = board.removePiece(target);
+		ChessPiece piece = (ChessPiece)board.removePiece(target);
+		piece.decreaseMoveCount();
 		board.placePiece(piece, source);
 		
 		if (capturedPiece != null) {
